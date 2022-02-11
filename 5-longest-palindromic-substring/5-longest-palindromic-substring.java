bc6d5540b44dcc2a4b9056329e6dfb83c3a5f5eb
woonji.kim <rladnswl30@naver.com>
2022-02-11 13:32:43 +0900

    Time: 51 ms (59.84%), Space: 54.8 MB (22.61%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+
+        String res = s.substring(0, 1);
+        for (int i = 0; i < s.length(); i++) {
+
+            // get longest palindrome with center of i
+            String tmp = search(s, i, i);
+            if (tmp.length() > res.length()) {
+                res = tmp;
+            }
+
+            // get longest palindrome with center of i, i+1
+            tmp = search(s, i, i + 1);
+            if (tmp.length() > res.length()) {
+                res = tmp;
+            }
+        }
+        return res;
+    }
+
+    public static String search(String s, int start, int end) {
+        while (start >= 0 && end <= s.length() - 1 && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+        }
+        return s.substring(start + 1, end);
+    }
+}
