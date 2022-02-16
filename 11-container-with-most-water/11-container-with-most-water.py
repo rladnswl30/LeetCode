05d3e4c46d3e0191f923edeaaacd38ec9e0d14ae
woonji.kim <rladnswl30@naver.com>
2022-02-17 02:06:19 +0900

    Time: 784 ms (66.96%), Space: 27.5 MB (46.42%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,17 +1,13 @@
-class Solution {
-    public int maxArea(int[] height) {
-        int max = 0;
-        int start = 0;
-        int end = height.length - 1;
-        while (start < end) {
-            max = Math.max(Math.min(height[start], height[end]) * (end - start), max);
-            if (height[start] < height[end]) {
-                start++;
-            } else {
-                end--;
-            }
-        }
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        result = 0
+        start = 0
+        end = len(height) - 1
+        while start < end:
+            result = max(min(height[start], height[end]) * (end - start), result)
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end -= 1
 
-        return max;
-    }
-}
+        return result
