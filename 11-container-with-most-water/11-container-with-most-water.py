5e3e120fb6f95d0d6cfd4651c6941d727a99e9e7
woonji.kim <rladnswl30@naver.com>
2022-02-18 01:40:27 +0900

    Time: 1002 ms (45.23%), Space: 27.6 MB (45.84%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -3,8 +3,10 @@ class Solution:
         result = 0
         start = 0
         end = len(height) - 1
+        
         while start < end:
             result = max(min(height[start], height[end]) * (end - start), result)
+            
             if height[start] < height[end]:
                 start += 1
             else:
