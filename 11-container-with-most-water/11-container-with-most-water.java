87e5c300eee9b320d1a0bfae6cf239377f5e4744
woonji.kim <rladnswl30@naver.com>
2022-02-18 01:41:30 +0900

    Time: 3 ms (91.89%), Space: 52.6 MB (80.76%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -3,8 +3,10 @@ class Solution {
         int max = 0;
         int start = 0;
         int end = height.length - 1;
+        
         while (start < end) {
             max = Math.max(Math.min(height[start], height[end]) * (end - start), max);
+            
             if (height[start] < height[end]) {
                 start++;
             } else {
