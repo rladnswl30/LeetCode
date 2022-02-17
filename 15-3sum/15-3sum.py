deb739fc235ace0ec006aa9cb403e433d6828474
woonji.kim <rladnswl30@naver.com>
2022-02-18 01:38:57 +0900

    Time: 1714 ms (36.20%), Space: 17.9 MB (46.88%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,6 +5,7 @@ class Solution:
         # print(nums)
 
         for i, val in enumerate(nums):
+            # 연속된 값이 동일하면 pass
             if i > 0 and val == nums[i - 1]:
                 continue
 
@@ -12,6 +13,7 @@ class Solution:
             right = len(nums) - 1
 
             while left < right:
+                # print(val, nums[left], nums[right])
                 allSum = val + nums[left] + nums[right]
                 if allSum < 0:
                     left += 1
@@ -19,8 +21,11 @@ class Solution:
                     right -= 1
                 else:
                     res.append([val, nums[left], nums[right]])
+                    # print(res)
                     left += 1
                     right -= 1
+
+                    # 연속된 값이 동일하면 다음 커서로 넘어간다
                     while nums[left - 1] == nums[left] and left < right:
                         left += 1
                     while nums[right + 1] == nums[right] and left < right:
