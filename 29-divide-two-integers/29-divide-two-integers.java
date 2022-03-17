14f7c274f3217a25b141f97d7bd496d74979c4d1
woonji.kim <rladnswl30@naver.com>
2022-03-18 03:22:57 +0900

    Time: 2 ms (71.73%), Space: 41.6 MB (36.13%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -1,42 +1,41 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        //Reduce the problem to positive long integer to make it easier.
-	//Use long to avoid integer overflow cases.
-	int sign = 1;
-	if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
-		sign = -1;
-	long ldividend = Math.abs((long) dividend);
-	long ldivisor = Math.abs((long) divisor);
-	
-	//Take care the edge cases.
-	if (ldivisor == 0) return Integer.MAX_VALUE;
-	if ((ldividend == 0) || (ldividend < ldivisor))	return 0;
-	
-	long lans = ldivide(ldividend, ldivisor);
-	
-	int ans;
-	if (lans > Integer.MAX_VALUE){ //Handle overflow.
-		ans = (sign == 1)? Integer.MAX_VALUE : Integer.MIN_VALUE;
-	} else {
-		ans = (int) (sign * lans);
-	}
-	return ans;
-}
+        boolean negative = false;
+        if (dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0) {
+            negative = true;
+        }
 
-private long ldivide(long ldividend, long ldivisor) {
-	// Recursion exit condition
-	if (ldividend < ldivisor) return 0;
-	
-	//  Find the largest multiple so that (divisor * multiple <= dividend), 
-	//  whereas we are moving with stride 1, 2, 4, 8, 16...2^n for performance reason.
-	//  Think this as a binary search.
-	long sum = ldivisor;
-	long multiple = 1;
-	while ((sum+sum) <= ldividend) {
-		sum += sum;
-		multiple += multiple;
-	}
-	//Look for additional value for the multiple from the reminder (dividend - sum) recursively.
-	return multiple + ldivide(ldividend - sum, ldivisor);
-}
+        long d = Math.abs((long) dividend);
+        long v = Math.abs((long) divisor);
+
+        if (v == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (d == 0 || d < v) {
+            return 0;
+        }
+
+        long res = recursive(d, v);
+
+        if (res > Integer.MAX_VALUE) {
+            res = negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        }
+        return negative ? (int)res * -1 : (int)res;
+    }
+
+    private static long recursive(long d, long v) {
+        if (d < v) {
+            return 0;
+        }
+
+        long sum = v;
+        long multiple = 1;
+
+        while (sum + sum <= d) {
+            sum += sum;
+            multiple += multiple;
+        }
+        return multiple + recursive(d - sum, v);
+    }
 }
