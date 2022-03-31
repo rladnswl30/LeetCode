7274ea3d82d71b7481f3bde42e5d83bcd68d7172
woonji.kim <rladnswl30@naver.com>
2022-04-01 05:45:40 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (55.59%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -4,22 +4,22 @@ class Solution {
     }
 
     public static int binarySearch(int[] nums, int target, int left, int right) {
-        if (left > right) {
+        if (left > right) { // cannot find target
             return -1;
         }
 
         int mid = (left + right) / 2;
-        if (nums[mid] == target) {
+        if (nums[mid] == target) { // find target
             return mid;
         }
 
-        if (nums[left] <= nums[mid]) { // left 탐색
+        if (nums[left] <= nums[mid]) { // search left
             if (nums[left] <= target && nums[mid] >= target) { // target contains left
                 return binarySearch(nums, target, left, mid - 1);
             } else { // target contains right
                 return binarySearch(nums, target,mid + 1, right);
             }
-        } else { // right 탐색
+        } else { // search right
             if (nums[mid] <= target && nums[right] >= target) { // target contains right
                 return binarySearch(nums, target, mid + 1, right);
             } else { // target contains left
