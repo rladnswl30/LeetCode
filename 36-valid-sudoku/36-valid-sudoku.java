f5fe3a566008d678ed656463282e415cdc452059
woonji.kim <rladnswl30@naver.com>
2022-04-15 12:28:40 +0900

    Time: 12 ms (33.94%), Space: 43.1 MB (82.00%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        System.out.println("================ row/col ================");
+        for (int i = 0; i < 9; i++) {
+            List<Character> rowCheck = new ArrayList<>();
+            List<Character> colCheck = new ArrayList<>();
+            for (int j = 0; j < 9; j++) {
+                char row = board[i][j];
+                char col = board[j][i];
+                if (row != '.' && rowCheck.contains(row)) {
+                    System.out.println("invalid: " + row);
+                    return false;
+                } else {
+                    rowCheck.add(row);
+                }
+
+                if (col != '.' && colCheck.contains(col)) {
+                    System.out.println("invalid: " + col);
+                    return false;
+                } else {
+                    colCheck.add(col);
+                }
+            }
+        }
+
+        System.out.println("================ grid ================");
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                List<Character> check = new ArrayList<>();
+                for (int k = 0; k < 3; k++) {
+                    for (int l = 0; l < 3; l++) {
+                        char gridNum = board[i + k][j + l];
+                        if (gridNum != '.' && check.contains(gridNum)) {
+                            System.out.println("invalid: " + gridNum);
+                            return false;
+                        } else {
+                            check.add(gridNum);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
