b6d5f5df7334376287c25573fefcac22e90555ac
woonji.kim <rladnswl30@naver.com>
2022-04-15 12:34:29 +0900

    Time: 13 ms (33.05%), Space: 43 MB (82.66%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -2,11 +2,16 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         System.out.println("================ row/col ================");
         for (int i = 0; i < 9; i++) {
+
             List<Character> rowCheck = new ArrayList<>();
             List<Character> colCheck = new ArrayList<>();
+
             for (int j = 0; j < 9; j++) {
+
                 char row = board[i][j];
                 char col = board[j][i];
+
+                // row
                 if (row != '.' && rowCheck.contains(row)) {
                     System.out.println("invalid: " + row);
                     return false;
@@ -14,6 +19,7 @@ class Solution {
                     rowCheck.add(row);
                 }
 
+                // col
                 if (col != '.' && colCheck.contains(col)) {
                     System.out.println("invalid: " + col);
                     return false;
@@ -26,15 +32,17 @@ class Solution {
         System.out.println("================ grid ================");
         for (int i = 0; i < 9; i += 3) {
             for (int j = 0; j < 9; j += 3) {
-                List<Character> check = new ArrayList<>();
+
+                List<Character> gridCheck = new ArrayList<>();
+
                 for (int k = 0; k < 3; k++) {
                     for (int l = 0; l < 3; l++) {
                         char gridNum = board[i + k][j + l];
-                        if (gridNum != '.' && check.contains(gridNum)) {
+                        if (gridNum != '.' && gridCheck.contains(gridNum)) {
                             System.out.println("invalid: " + gridNum);
                             return false;
                         } else {
-                            check.add(gridNum);
+                            gridCheck.add(gridNum);
                         }
                     }
                 }
