faa170a65fcff6b4c22287db91482adf6e9da83d
woonji.kim <rladnswl30@naver.com>
2022-04-22 12:58:15 +0900

    Time: 9 ms (43.46%), Space: 42.1 MB (56.91%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -7,18 +7,20 @@ class Solution {
         String res = "1";
 
         for (int i = 1; i < n; i++) {
-            StringBuilder b = new StringBuilder();
+            
+            StringBuilder sb = new StringBuilder();
             for (int j = 0; j < res.length(); j++) {
+
                 int count = 1;
                 while (j + 1 < res.length() && res.charAt(j) == res.charAt(j + 1)) {
                     count++;
                     j++;
                 }
-                b.append(count).append(res.charAt(j));
+                sb.append(count).append(res.charAt(j));
             }
-            res = b.toString();
-
+            res = sb.toString();
         }
+        
         return res;
     }
 }
